588ece1017211db5d78c228fa767f795bea665ef
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2020-05-01 02:16:30 +0300

    #8

--- js/script.js
+++ js/script.js
@@ -1,19 +1,62 @@
 $(function(){
 
+  //Fixed Header фиксированнная шапка после прокрутки интро
 let header = $("#header");
 let intro = $("#intro");
-let introH ;
+let introH = intro.innerHeight();
 let scrollPos =$(window).scrollTop();
-$(window).on("scroll load resize",function(){
+let nav=$("#nav")
+let navToggle=$("#navToggle");
+
+checkScroll(scrollPos,introH);
+$(window).on("scroll resize",function(){
+  //следим за событием при скроле, загрузки и мобильных телефонов
 introH = intro.innerHeight();
 scrollPos=$(this).scrollTop();
-if(scrollPos >introH ){
-header.addClass("fixed");
-}else{
-  header.removeClass("fixed");
+
+checkScroll(scrollPos,introH);
+
+});
+
+function checkScroll(scrollPos,introH){
+  if(scrollPos >introH ){
+    header.addClass("fixed");
+    }else{
+      header.removeClass("fixed");
+    }
 }
 
+//smoth scroll
+$("[data-scroll]").on("click",function(event){
+event.preventDefault();
+let elementID=$(this).data('scroll');
+let elementOffset = $(elementID).offset().top;
+
+nav.removeClass("show");
+
+$("html,body").animate({
+  scrollTop:elementOffset - 70
+},700);
+});
+
+// navToggle//
+
+
+navToggle.on("click",function(event){
+event.preventDefault();
+nav.toggleClass("show")
+});
+
+/*Reviews <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>*/ 
+let slider=$("#reviewsSlider");
+slider.slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade:true,
+  arrows:false,
+  dots:true
 });
 
 
-});
+});//закрывает все функции
